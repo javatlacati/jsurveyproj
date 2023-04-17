2a125819a6a9b4133073c98b8fb775ed3464300c
javatlacati <scherzo_16@hotmail.com>
2023-04-17 10:18:40 -0500

    template client

--- jsurveyforms/src/main/java/org/javapro/MyRemoteService.java
+++ jsurveyforms/src/main/java/org/javapro/MyRemoteService.java
@@ -1,12 +1,11 @@
 package org.javapro;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.javapro.survey.model.Survey;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-import java.util.List;
-import java.util.Set;
 
 /**
  * To use it via injection.
@@ -21,17 +20,10 @@ import java.util.Set;
  * }
  * }
  */
-@RegisterRestClient(baseUri = "https://stage.code.quarkus.io/api")
+@RegisterRestClient(baseUri = "http://localhost:8080")
 public interface MyRemoteService {
 
     @GET
-    @Path("/extensions")
-    Set<Extension> getExtensionsById(@QueryParam("id") String id);
-
-    class Extension {
-        public String id;
-        public String name;
-        public String shortName;
-        public List<String> keywords;
-    }
+    @Path("/template")
+    Survey getTemplateById(@QueryParam("id") String id);
 }
