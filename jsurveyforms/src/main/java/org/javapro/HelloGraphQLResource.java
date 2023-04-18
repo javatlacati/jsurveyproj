2ada6fd303bd8315c554e9d8f93e07ace97279cc
javatlacati <scherzo_16@hotmail.com>
2023-04-18 11:27:00 -0500

    added graphql controller

--- jsurveyforms/src/main/java/org/javapro/HelloGraphQLResource.java
+++ jsurveyforms/src/main/java/org/javapro/HelloGraphQLResource.java
@@ -4,13 +4,40 @@ import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
+import org.javapro.formtemplate.model.SurveyTemplate;
+import org.javapro.survey.model.Survey;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 @GraphQLApi
 public class HelloGraphQLResource {
+    @Inject
+    TemplateService templateService;
+
+//    @Inject
+//    Vertx vertx;
+//
+//    private WebClient webClient;
+
+    @PostConstruct
+    void initialize(){
+//        this.webClient.create(vertx, new WebClientOptions().setDefaultHost("localhost").setDefaultPort(8081).setSsl(false).setTrustAll(true));
+    }
 
     @Query
     @Description("Say hello")
     public String sayHello(@DefaultValue("World") String name) {
         return "Hello " + name;
+    }
+
+    @Query
+    @Description("Retrieves a template")
+    public SurveyTemplate getTemplate(Long id) {
+        return templateService.getTemplateById(Long.toString(id));
+    }
+
+    public void saveNewSurvey(Survey survey){
+
     }
 }
