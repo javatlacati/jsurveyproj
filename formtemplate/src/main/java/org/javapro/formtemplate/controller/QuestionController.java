4b77f0968198e77ddcf9fb56a0300eb4d87fea91
Ruslan Lopez Carro <scherzo_16@hotmail.com>
2023-05-27 14:02:24 -0600

    adding missing annotation for Question controller

--- formtemplate/src/main/java/org/javapro/formtemplate/controller/QuestionController.java
+++ formtemplate/src/main/java/org/javapro/formtemplate/controller/QuestionController.java
@@ -1,7 +1,9 @@
 package org.javapro.formtemplate.controller;
 
 import org.javapro.formtemplate.QuestionService;
+import org.javapro.formtemplate.controller.strategy.QuestionUpdateStrategyFactory;
 import org.javapro.formtemplate.model.Question;
+import org.javapro.formtemplate.model.QuestionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +24,9 @@ public class QuestionController {
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private QuestionUpdateStrategyFactory questionUpdateStrategyFactory;
+
     @GetMapping("/questions")
     public List<Question> findAll() {
         return questionService.findAll();
@@ -40,22 +45,24 @@ public class QuestionController {
     @PostMapping("/question")
     @ResponseBody
     public Question createQuestion(@RequestBody Question question) {
+        System.out.println("Saving question:" + question);
         return questionService.save(question);
     }
 
     @PatchMapping("/question/{questionId}")
-    public Optional<Question> updateQuestion(@PathVariable Long questionId, Question question) {
+    public Optional<Question> updateQuestion(@PathVariable Long questionId, @RequestBody Question question) {
         Optional<Question> possibleQuestion = questionService.findById(questionId);
         if (possibleQuestion.isPresent()) {
             Question existingQuestion = possibleQuestion.get();
-            updateQuestionFields(existingQuestion,question);
-            return Optional.of(questionService.save(existingQuestion));
+            return Optional.ofNullable(updateQuestionFields(existingQuestion, question));
         } else {
             return Optional.of(createQuestion(question));
         }
     }
 
-    private void updateQuestionFields(Question existingQuestion, Question question) {
-        //TODO use strategy design pattern
+    private Question updateQuestionFields(Question existingQuestion, Question question) {
+        System.out.println("updating question:" + existingQuestion + "\nwith question:" + question);
+        QuestionType existingQuestionType = existingQuestion.getType();
+        return questionUpdateStrategyFactory.findByType(existingQuestionType).updateQuestionFields(existingQuestion, question);
     }
 }
